3204d25805415df3185af49b161fddb5ce1f82f1
Suchi Bansal <suchi.bansal@10i.co.in>
2022-07-13 18:52:25 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (6.32%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,46 +1,36 @@
 class Solution {
+
     public int findMin(int[] nums) {
-        
-        if(nums.length == 0){
+        if (nums.length == 0) {
             return 0;
         }
-        if(nums.length == 1){
+        if (nums.length == 1) {
             return nums[0];
         }
-        if(nums[0]<nums[nums.length-1]){
-             return nums[0];
+        if (nums[0] < nums[nums.length - 1]) {
+            return nums[0];
         }
-      
-        return binarySearch(nums,0,nums.length-1);
-        
+
+        return binarySearch(nums, 0, nums.length - 1);
     }
-    
-   private int binarySearch(int[] nums,int start, int end){
-       
-       if(start==end){
-           return nums[start];
-       }
-       if(nums[start]<nums[end]){
-           return nums[start];
-       }
-       int mid = start+(end-start)/2;
-        
-        if(mid>start && nums[mid] < nums[mid-1]){
-            
-          return nums[mid];
-            
+
+    private int binarySearch(int[] nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
         }
-        
-        if(mid<end && nums[mid]>nums[end]){
-            
-           return binarySearch(nums, mid+1,end);
-            
-        }else{
-            
-          return binarySearch(nums, start,mid-1);
-            
+        if (nums[start] < nums[end]) {
+            return nums[start];
         }
-       
+        int mid = start + (end - start) / 2;
+
+        if (mid > start && nums[mid] < nums[mid - 1]) {
+            return nums[mid];
+        }
+
+        if (mid < end && nums[mid] > nums[end]) {
+            return binarySearch(nums, mid + 1, end);
+        } else {
+            return binarySearch(nums, start, mid - 1);
+        }
     }
-       
 }
