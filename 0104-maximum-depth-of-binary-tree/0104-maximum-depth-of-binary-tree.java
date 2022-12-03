e1abfb3061700c915385652adf220b0f064952f8
Suchi Bansal <suchi.bansal@10i.co.in>
2022-12-03 20:57:06 +0530

    Time: 3 ms (7.66%), Space: 41.7 MB (97.81%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;    
+    
+    public void dfs(TreeNode root) {
+        
+        Stack<Pair<TreeNode, Integer>> s = new Stack<>();
+        s.push(new Pair<>(root, 1));
+
+        while(!s.empty()) {
+            Pair<TreeNode, Integer> node = s.pop();
+            
+            if(node.getValue() > max) {
+                max = node.getValue();
+            }
+            
+            TreeNode n = node.getKey();
+
+            if(n.left != null) {
+                s.push(new Pair<>(n.left, node.getValue()+1));
+            }
+            if(n.right != null) {
+                s.push(new Pair<>(n.right, node.getValue()+1));
+            }
+            
+        }
+        
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        dfs(root);
+        return max;
+//         if (max < count) {
+//             max = count;
+//         }
+
+//         maxDepth(root.left, count+1);
+//         maxDepth(root.right, count+1);
+
+//         return max;
+    }
+}
