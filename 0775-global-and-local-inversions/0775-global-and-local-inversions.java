69eaebce336b5ce3febecdbcfc40767460a294a2
Suchi Bansal <suchi.bansal@10i.co.in>
2022-12-26 16:10:01 +0530

    Time: 1 ms (100.00%), Space: 52 MB (80.52%) - LeetHub

---
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public boolean isIdealPermutation(int[] nums) {
+        int max = -1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            max = Math.max(max, nums[i]);
+            if (max > nums[i + 2]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
