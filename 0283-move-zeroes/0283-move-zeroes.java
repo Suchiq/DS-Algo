c86c7f5992f0f7db3645df1832f89cb48ae9d786
Suchi Bansal <suchi.bansal@10i.co.in>
2022-12-30 12:04:56 +0530

    Time: 2 ms (79.56%), Space: 43.6 MB (91.16%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+
+        while (i <= j && j < nums.length) {
+            if (nums[j] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+            j++;
+        }
+    }
+}
