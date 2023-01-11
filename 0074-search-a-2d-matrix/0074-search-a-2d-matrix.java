bc77dbd702ed4e59f01e9dbd52e33d110038d3f0
Suchi Bansal <suchi.bansal@10i.co.in>
2023-01-11 11:49:51 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (87.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,19 +1,45 @@
 class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length - 1;
-        int column = matrix[0].length - 1;
+        //row and column wise search
 
-        int i = 0, j = matrix[0].length - 1;
-        
-        while (i <= row && j >= 0) {
-                
-            if (matrix[i][j] == target) {
+        // int row = matrix.length;
+        //int i = 0;
+        //int j = matrix[0].length - 1;
+
+        // while (i < row && j >= 0) {
+        //  if (matrix[i][j] == target) {
+        //     return true;
+        // } else if (matrix[i][j] > target) {
+        //      j--;
+        // } else {
+        //     i++;
+        // }
+        //  }
+        //  return false;
+
+        //solution 2 -- binary search in matrix
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // totatal no of element in sorted matrix
+
+        int total = (row * col) - 1;
+        int low = 0;
+        int high = total;
+
+        while (low <= high) {
+            int mid = (low + (high - low) / 2);
+
+            if (matrix[mid / col][mid % col] == target) {
                 return true;
-            } else if (matrix[i][j] > target) {
-                j--;
-            } else if (matrix[i][j] < target) {
-                i++;
+            }
+
+            if (matrix[mid / col][mid % col] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
