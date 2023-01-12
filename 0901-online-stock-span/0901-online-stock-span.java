af6db7977ab545ec2d37d5315a70e9903db93866
Suchi Bansal <suchi.bansal@10i.co.in>
2023-01-12 21:53:51 +0530

    Time: 45 ms (91.47%), Space: 50.3 MB (85.88%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<int[]>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span = span + stack.pop()[1];
+        }
+
+        stack.push(new int[] { price, span });
+        return span;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
