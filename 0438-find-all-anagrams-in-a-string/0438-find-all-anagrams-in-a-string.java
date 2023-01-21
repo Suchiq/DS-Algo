63f5f0be1a6c5530d1c561378c0e947f92fb3732
Suchi Bansal <suchi.bansal@10i.co.in>
2023-01-21 16:19:45 +0530

    Time: 30 ms (51.60%), Space: 43.6 MB (53.13%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    public List<Integer> findAnagrams(String s, String p) {
+        //sliding window size
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        List<Integer> resList = new ArrayList<>();
+        int k = p.length();
+
+        //make sliding window of k size
+
+        for (int i = 0; i < k; i++) {
+            //save pattern in hasmap with count frequency
+            if (map.containsKey(p.charAt(i))) {
+                map.put(p.charAt(i), map.get(p.charAt(i)) + 1);
+            } else {
+                map.put(p.charAt(i), 1);
+            }
+        }
+        int i = 0;
+        int j = 0;
+        int UniqInMap = map.size();
+
+        while (j < s.length()) {
+            char ch = s.charAt(j);
+
+            // System.out.println(ch);
+            //System.out.println(UniqInMap+"");
+
+            if (map.containsKey(ch)) {
+                // minus frequency of that ch in the map because we have found that
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0) {
+                    UniqInMap--;
+                }
+            }
+            //  System.out.println(k+"");
+
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                //window hit record calulation
+
+                if (UniqInMap == 0) {
+                    //means anagarm found in that window
+
+                    resList.add(i);
+                }
+                //remove prev from window add new char to window
+                //System.out.println(i+"");
+
+                char prev = s.charAt(i);
+                //System.out.println(prev + "");
+
+                if (map.containsKey(prev)) {
+                    map.put(prev, map.get(prev) + 1);
+
+                    if (map.get(prev) == 1) {
+                        UniqInMap++;
+                    }
+                }
+
+                i++;
+                j++;
+            }
+        }
+        return resList;
+    }
+}
