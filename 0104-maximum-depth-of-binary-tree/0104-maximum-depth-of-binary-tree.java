f182df5cc29703ab5988079d2c7de799f8df62bb
Suchi Bansal <suchibansal2007@gmail.com>
2024-09-24 08:43:22 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (48.67%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,50 +14,24 @@
  * }
  */
 class Solution {
-    int max = 0;
-
-    public void dfs(TreeNode root) {
-        Stack<Pair<TreeNode, Integer>> s = new Stack<>();
-        s.push(new Pair<>(root, 1));
-
-        while (!s.empty()) {
-            Pair<TreeNode, Integer> node = s.pop();
-
-            if (node.getValue() > max) {
-                max = node.getValue();
-            }
-
-            TreeNode n = node.getKey();
-
-            if (n.left != null) {
-                s.push(new Pair<>(n.left, node.getValue() + 1));
-            }
-            if (n.right != null) {
-                s.push(new Pair<>(n.right, node.getValue() + 1));
-            }
-        }
-    }
+   
 
     public int maxDepth(TreeNode root) {
-        // dfs(root);
-        //return max;
-        //int max = 1;
-        return findHeight(root);
-    }
-
-    int findHeight(TreeNode root) {
         
-           
-        if (root == null) {
-            return 0;
-        }
-         
-        // dfs(root);
-        //return max;
-      
-      int left =  findHeight(root.left);
-      int right = findHeight(root.right);
-
-        return Math.max(left,right)+1;
+        return maxDepthHelper(root,0);
+       
     }
+
+    public int maxDepthHelper(TreeNode root, int depth){
+        
+        if(root == null){
+            return depth;
+        }
+        
+        int leftHeight = maxDepthHelper(root.left,depth+1);
+        int rightHeight = maxDepthHelper(root.right,depth+1);
+        
+        
+        return Math.max(leftHeight,rightHeight);
+    }   
 }
