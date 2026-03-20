f8691b90722b8151d3bea74e14fc3407b9f2518c
Suchi Bansal <suchibansal2007@gmail.com>
2026-03-20 14:44:37 +0530

    Time: 2 ms (99.58%), Space: 44.3 MB (87.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (char ch1 : s.toCharArray()) {
+            count[ch1 - 'a']++;
+        }
+
+        for (char ch2 : t.toCharArray()) {
+            count[ch2 - 'a']--;
+        }
+        for (int a : count) {
+
+            if (a != 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
