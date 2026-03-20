d2705cc048d193798b40d1cb338b8d299e6f8f9e
Suchi Bansal <suchibansal2007@gmail.com>
2026-03-20 15:22:03 +0530

    Time: 2 ms (99.23%), Space: 44.4 MB (63.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,31 @@
-  class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
+        char[] charA = s.toCharArray();
 
-        String fixedString = "";
-         
-         for(int i =0; i< s.length();i++){
-           if(isAlphanumeric(s.charAt(i))){
-             fixedString = fixedString + s.charAt(i);
-           }
-         }
+        int i = 0;
+        int j = charA.length - 1;
 
-         fixedString = fixedString.toLowerCase(); 
-         int i =0;
-         int j = fixedString.length()-1;
+        while (i < j) {
 
-         while(i<=j){
-            if(fixedString.charAt(i)!=fixedString.charAt(j)){
-                
+            if (!Character.isLetterOrDigit(charA[i])) {
+                i++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(charA[j])) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(charA[i]) == Character.toLowerCase(charA[j])) {
+                i++;
+                j--;
+
+            } else {
                 return false;
             }
-             i++;
-             j--;
-         }
+        }
         return true;
     }
 
-    public boolean isAlphanumeric(Character ch){
-        if(Character.isLetterOrDigit(ch)){
-            return true;
-        }
-        return false;
-    }
 }
